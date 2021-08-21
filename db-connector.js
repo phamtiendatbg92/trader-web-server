c87b4e80594af9c427c66bcf2b5875b59fe2a025
datpt <dat1.phamtien@toshiba.co.jp>
2021-08-21 22:20:51 +0700

    refactor environment variable to use var in heroku

--- db-connector.js
+++ db-connector.js
@@ -27,8 +27,11 @@ const hashTagSchema = new Schema({
 });
 
 const HashTagModel = mongoose.model('HashTags', hashTagSchema);
+const uri = process.env.MONGODB_URI;
 
-mongoose.connect('mongodb://localhost:27017/trader', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/trader', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
